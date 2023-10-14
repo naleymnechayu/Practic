8b02408e2540f3c1eaac3938ec37786dc1c499ac
sabotl <pif27011@mail.ru>
2023-10-14 22:17:09 +0300

    6.4add

---
+++ practic6/6.4/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace task
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+    }
+    public class Triangle : Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+
+        public override double CalculateArea()
+        {
+            double semiPerimeter = CalculatePerimeter() / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - SideA) * (semiPerimeter - SideB) * (semiPerimeter - SideC));
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+
+        public string IsRightTriangle()
+        {
+            if (SideA * SideA + SideB * SideB == SideC * SideC || SideA * SideA + SideC * SideC == SideB * SideB || SideC * SideC + SideB * SideB == SideA * SideA)
+            {
+                return "Прямоугольный";
+            }
+            else if (SideA * SideA + SideB * SideB < SideC * SideC || SideA * SideA + SideC * SideC < SideB * SideB || SideC * SideC + SideB * SideB < SideA * SideA)
+            {
+                return "Тупоугольный";
+            }
+            else
+            {
+                return "Остроугольный";
+            }
+        }
+    }
+}
---
+++ practic6/6.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        task.Circle circle = new task.Circle() { Radius = 15 };
+        task.Rectangle rectangle = new task.Rectangle() { Width = 15, Height = 10 };
+        task.Triangle triangle = new task.Triangle() { SideA = 2, SideB = 3, SideC = 2 };
+
+        Console.WriteLine($"Площадь круга: {circle.CalculateArea()}");
+        Console.WriteLine($"Периметр круга: {circle.CalculatePerimeter()}");
+
+        Console.WriteLine($"Площадь прямоугольника: {rectangle.CalculateArea()}");
+        Console.WriteLine($"Периметр прямоугольника: {rectangle.CalculatePerimeter()}");
+
+        Console.WriteLine($"Площадь треугольника: {triangle.CalculateArea()}");
+        Console.WriteLine($"Периметр треугольника: {triangle.CalculatePerimeter()}");
+        Console.WriteLine($"Треугольник является: {triangle.IsRightTriangle()}");
+
+        Console.ReadLine();
+    }
+}
